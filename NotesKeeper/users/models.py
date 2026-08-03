3860bc0a81eb7596ced0ec516f8ac90e48b5af98
VijayBokade-26 <bokadevijayvb@gmail.com>
2026-08-03 19:03:57 +0530

    trying to add models

--- NotesKeeper/users/models.py
+++ NotesKeeper/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    phone_no = models.CharField(max_length=15, blank = True, null = True)
+
+    class Meta:
+        pass
+
+
+
