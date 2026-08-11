8e587094ee63747de16b80674b06cb6d392f36a0
VijayBokade-26 <bokadevijayvb@gmail.com>
2026-08-11 19:02:36 +0530

    removed from users app and added into authentication app (JWT auth process)

--- NotesKeeper/users/urls.py
+++ NotesKeeper/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from . import views
-from rest_framework_simplejwt.views import TokenVerifyView,TokenObtainPairView, TokenRefreshView
+# from rest_framework_simplejwt.views import TokenVerifyView,TokenObtainPairView, TokenRefreshView
+
 urlpatterns = [
     # path("notes/",views.users.as_view() ),
-    path("auth/login/",TokenObtainPairView.as_view() ),
-    path("auth/token/refresh",TokenRefreshView.as_view() ),
+    # path("auth/login/",TokenObtainPairView.as_view() ),
+    # path("auth/token/refresh",TokenRefreshView.as_view() ),
 
 
 ]
