6c4d7c170e0122ece54f5b655389fb8e82826012
VijayBokade-26 <bokadevijayvb@gmail.com>
2026-08-11 19:03:43 +0530

    added register logic into view

--- NotesKeeper/users/serializers.py
+++ NotesKeeper/users/serializers.py
@@ -2,14 +2,25 @@ from .models import User
 from rest_framework import serializers
 
 class SignUpSerializers(serializers.ModelSerializer):
-    confirm_password = serializers.CharField(write_only = True)
+    confirm_password = serializers.CharField(write_only = True,required = True)
 
     class Meta:
         model = User
         fields = ["first_name", "last_name", "email", "phone", "password", "confirm_password", ]
 
-    def validate(self,attributes):
-        pass
+    def validate(self,attrs):
+        password = attrs.get("password")
+        self.confirm_password  = attrs.pop("confirm_password",None)
 
+        if password != self.confirm_password:
+            raise serializers.ValidationError({"confirm_password": "Password do not match"})
+        return attrs
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
 
     
