ee3f7551b8ab3d7c91bf91500820da1f0619a872
VijayBokade-26 <bokadevijayvb@gmail.com>
2026-08-11 19:03:08 +0530

    updated serializer and views logic

--- NotesKeeper/authentication/views.py
+++ NotesKeeper/authentication/views.py
@@ -3,6 +3,8 @@ from users import models as UserModel
 from users import serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import exceptions
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
 class SignUpView:
     queryset = UserModel.User.objects.all() 
@@ -11,9 +13,21 @@ class SignUpView:
     allowed_methods = ['POST']
 
     def post(self,request, *args, **kwargs):
-        email = request.data.get("email")
-        if UserModel.objects.filter(email_i_exact = email).exists():
-            raise exceptions.bad_request(data = {'error': "Email is already exists!"})
+        serializer = self.serializer_class(data = request.data)
+        serializer.is_valid(raise_exception = True)
+        user = serializer.save()
+        return Response(
+            {
+              "message": "User registerd successfully!",
+              "user" : self.serializer_class(user).data      
+            }, status = status.HTTP_201_CREATED
+        )
+        
+
+        
+        
+        # self.queryset.objects.create)
+        
 
         
         
