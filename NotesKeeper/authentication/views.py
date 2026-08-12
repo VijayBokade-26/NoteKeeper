a7f6957a47ea799cde3f29ae8cab3c1546e37358
VijayBokade-26 <bokadevijayvb@gmail.com>
2026-08-12 19:20:20 +0530

    added try except

--- NotesKeeper/authentication/views.py
+++ NotesKeeper/authentication/views.py
@@ -5,31 +5,42 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import exceptions
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 # Create your views here.
-class SignUpView:
+class SignUpView(APIView):
     queryset = UserModel.User.objects.all() 
     serializer_class = serializers.SignUpSerializers
     permission_classes = (IsAuthenticated,)
-    allowed_methods = ['POST']
+    allowed_methods = ["GET", "POST"]
 
     def post(self,request, *args, **kwargs):
-        serializer = self.serializer_class(data = request.data)
-        serializer.is_valid(raise_exception = True)
-        user = serializer.save()
-        return Response(
-            {
-              "message": "User registerd successfully!",
-              "user" : self.serializer_class(user).data      
-            }, status = status.HTTP_201_CREATED
-        )
-        
+        try:
+            serializer = self.serializer_class(data = request.data)
+            serializer.is_valid(raise_exception = True)
+            user = serializer.save()
+            return Response(
+                {
+                "message": "User registerd successfully!",
+                "user" : self.serializer_class(user).data      
+                }, status = status.HTTP_201_CREATED
+            )
+        except Exception as e:
+            return Response({
+                    "error": str(e),
+                },
+                status=status.HTTP_400_BAD_REQUEST,            
+            )        
 
-        
-        
-        # self.queryset.objects.create)
-        
+class LoginView(APIView):
+    queryset = UserModel.User.objects.all()
+    serializer_class = serializers.LoginSerializers
+    permission_class = (IsAuthenticated,)
+    Allowed_methods = ["GET", "POST"]   
 
-        
+    def post(self, request, *args, **kwargs):
+        request. 
+
+           
         
             
 
