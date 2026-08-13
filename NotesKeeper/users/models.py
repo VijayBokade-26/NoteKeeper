2159e5faccca70a765ac70a80bbd8452634ff2ee
VijayBokade-26 <bokadevijayvb@gmail.com>
2026-08-13 19:08:35 +0530

    added is_active user field

--- NotesKeeper/users/models.py
+++ NotesKeeper/users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     phone_no = models.CharField(max_length=15, blank = True, null = True)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=False)
     updated_at = models.DateTimeField(auto_now=True)
     is_deleted = models.BooleanField(default=False)
 
