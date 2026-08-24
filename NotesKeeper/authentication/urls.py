ed2dcdf5198c10e0c587a758ad637b592641911f
VijayBokade-26 <bokadevijayvb@gmail.com>
2026-08-24 19:15:36 +0530

    added nothing

--- NotesKeeper/authentication/urls.py
+++ NotesKeeper/authentication/urls.py
@@ -4,12 +4,12 @@ from rest_framework_simplejwt.views import TokenVerifyView,TokenObtainPairView, 
 urlpatterns = [
     # path("notes/",views.users.as_view() ),
     path("signup/", views.SignUpView.as_view()),
-    path("login/",views.Login.as_view() ),
+    path("login/",views.LoginView.as_view() ),
     path("/token/refresh",TokenRefreshView.as_view() ),
     path("/token/verify",TokenVerifyView.as_view() ),
-    path("logout/",views.Logout.as_view()),
-    path("reset_password/",views.ResetPassword.as_view()),
+    # path("logout/",views.Logout.as_view()),
+    # path("reset_password/",views.ResetPassword.as_view()),
     # path("/",views.ResetPassword.as_view()),
     # path("forget_password/",views.ResetPassword.as_view()),
     
-]
+] 
